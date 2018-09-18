2c562f2aa07df9208c6d9e619d1d41d0b216adf6
Sotnikov, Dmitri <dmitri.sotnikov@gmail.com>
2018-09-18 17:00:17 -0400

    support for adding and removing events

--- env/dev/cljs/ajenda/test_page.cljs
+++ env/dev/cljs/ajenda/test_page.cljs
@@ -2,12 +2,36 @@
   (:require [ajenda.core :as ajenda]
             [reagent.core :as r]))
 
-;; -------------------------
-;; Views
+(def events (r/atom [{:title "Test 1"
+                      :start (js/Date.)
+                      :tip   "tip1"}]))
+
+(defn add-event! [events event]
+  (swap! events conj event))
 
 (defn home-page []
-  [:div [:h2 "Welcome to Ajenda Test Page"]
-   [ajenda/calendar ajenda/events  #_{:events events-fn}]])
+  [:div
+   [:p (str @events)]
+   [ajenda/calendar
+    {:header       {:left   "prev, next today"
+                    :center "title"
+                    :right  "month,agendaWeek,agendaDay"}
+     :selectable   true
+     :events       (fn [start end timezone callback]
+                     (callback @events))
+     :event-render (fn [event element]
+                     (.attr element "title" (.-tip event)))
+     :event-click  (fn [event js-event view]
+                     (js/console.log event)
+                     event
+                     nil)
+     :select       (fn [start end event view]
+                     (let [event {:title (str "Test " (rand-int 1000))
+                                  :start start
+                                  :end   end
+                                  :tip   "tip1"}]
+                       (add-event! events event)
+                       event))}]])
 
 (defn mount-root []
   (r/render [home-page] (.getElementById js/document "app")))
--- src/cljs/ajenda/core.cljs
+++ src/cljs/ajenda/core.cljs
@@ -1,115 +1,58 @@
 (ns ajenda.core
-  (:require [reagent.core :as r]))
+  (:require
+    [clojure.string :as string]
+    [clojure.walk :refer [postwalk]]
+    [reagent.core :as r]))
 
 (defn $ [this]
-  (-> this
-      r/dom-node
-      js/$))
+  (-> this r/dom-node js/$))
 
-(def events (r/atom (vec (repeatedly
-                        10
-                        (fn []
-                          (let [start (+ (- (* 1000 60 60 24 30) (rand-int (* 1000 60 60 24 60))) (.valueOf (.now js/Date)))
-                                end  (+ start (rand-int (* 1000 60 60 12)))
-                                id (random-uuid)]
-                            {:id id
-                             :title (str id)
-                             :start (js/moment start)
-                             :end (js/moment end)}))))))
+(defn ->camel-case [s]
+  (string/replace s #"-{1,}\b." #(when-let [c (last %)] (.toUpperCase c))))
 
-(def opts (r/atom {:view "agendaWeek"
-                   :date (.valueOf (.now js/Date))}))
+(defn events-handler [f]
+  (fn [start end timezone callback]
+    (f start end timezone #(callback (clj->js %)))))
 
-(defn events-fn [cal-start cal-end timezone callback]
-  (callback (clj->js
-             (filterv
-              (fn [{:keys [start end]}]
-                (or (< (.valueOf cal-start) start (.valueOf cal-end))
-                    (< (.valueOf cal-start) end (.valueOf cal-end))))
-              @events))))
+(defn wrap-event-click
+  "removes the event from calendar when the click handler returns nil"
+  [f calendar]
+  (fn [event js-event view]
+    (let [id (.-_id event)]
+      (when-not (f event js-event view)
+        (.fullCalendar calendar "removeEvents" id)))))
 
-(def cal-opts
-  {:events events-fn
-   #_#_  :defaultView "agendaWeek"
-   :header {:left "month,agendaWeek,agendaDay"
-            :center "title"}
-   :height "auto"
-   :editable true
-   :eventDrop (fn [event delta revert parent-ev ui-obj view]
-                (let [event (js->clj event :keywordize-keys true)]
-                  (swap! events #(mapv (fn [{:keys [id] :as e}] (if (= id (:id event)) event e)) %))))
-   :eventResize (fn [event delta revert parent-ev ui-obj view]
-                  (let [event (js->clj event :keywordize-keys true)]
-                    (swap! events #(mapv (fn [{:keys [id] :as e}] (if (= id (:id event)) event e)) %))))
-   :dayClick (fn [date parent-ev view]
-               (println "Clicked: " (.format date)))})
+(defn wrap-rerender-events
+  "calls the select function and paints the even with the result"
+  [f calendar]
+  (fn [& args]
+    (when-let [event (apply f args)]
+      (.fullCalendar calendar "renderEvent" (clj->js event)))))
 
-(defn format-cal-opts [opts]
-  (-> opts
-      (or {})
+(defn rename-keys [opts]
+  (postwalk
+    (fn [node]
+      (if (keyword? node)
+        (keyword (->camel-case (name node)))
+        node))
+    opts))
+
+(defn parse-opts [calendar opts]
+  (-> (rename-keys opts)
+      (update :eventClick wrap-event-click calendar)
+      (update :select wrap-rerender-events calendar)
+      (update :events events-handler)
       (clj->js)))
 
-(defn parse-cal-opts [this opts]
-  (if (true? (:skip-parse opts))
-    (format-cal-opts opts)
-    (-> opts
-        (update :dayClick (fn [orig-fn]
-                            (fn [d p v]
-                              (orig-fn d p v))))
-        (format-cal-opts))))
-
-;; -------------------------
-;; Components
-
-(defn get-track
-  [track-fn & dereffables]
-  (apply r/track
-         (comp #(apply track-fn %)
-               #(mapv deref %&))
-         dereffables))
-
-(defn get-track!
-  [track-fn & dereffables]
-  (apply r/track!
-         (comp #(apply track-fn %)
-               #(mapv deref %&))
-         dereffables))
-
-(defn register-track!
-  [registry-atom track-fn & dereffables]
-  (let [tracker (apply get-track! track-fn dereffables)]
-    (swap! registry-atom (fnil conj #{}) tracker)
-    tracker))
-
-(defn dispose-all!
-  [registry-atom]
-  (doseq [tracker @registry-atom]
-    (r/dispose! tracker))
-  (reset! registry-atom #{}))
-
-(defn calendar [events opts {:keys [set-view set-date]}]
-  (let [view (get-track :view opts)
-        track-registry (atom #{})
-        track! (partial register-track! track-registry)]
-      (r/create-class
-       {:display-name "calendar"
-        :component-did-mount (fn [this]
-                               (.fullCalendar
-                                ($ this)
-                                {:viewRender (fn [new-view element]
-                                               (set-view (aget new-view "name"))
-                                               (when-let [new-date (.fullCalendar ($ this) "getDate")]
-                                                 (set-date new-date)))}))
-        :component-did-update (fn [this _]
-                                (println "Calendar did update")
-                                (-> this
-                                    $
-                                    (.fullCalendar "render")))
-        :component-will-unmount (fn [this]
-                                  (dispose-all! track-registry)
-                                  (-> this
-                                      $
-                                      (.fullCalendar "destroy")))
-        :reagent-render
-        (fn [_]
-          [:div.calendar])})))
+(defn calendar [opts]
+  (let []
+    (r/create-class
+      {:display-name           "calendar"
+       :component-did-mount    (fn [this]
+                                 (let [calendar ($ this)]
+                                   (.fullCalendar calendar (parse-opts calendar opts))))
+       :component-did-update   (fn [this _]
+                                 (-> this $ (.fullCalendar "render")))
+       :component-will-unmount (fn [this]
+                                 (-> this $ (.fullCalendar "destroy")))
+       :reagent-render         (fn [_] [:div.calendar])})))
