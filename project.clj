cb816bc447baa67149c948595c9e0309dec2bd2f
Sotnikov, Dmitri <dmitri.sotnikov@gmail.com>
2018-12-03 12:10:49 -0500

    fixed path to moment

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ajenda "0.1.13"
+(defproject ajenda "0.1.14"
   :description "A Reagent Wrapper for Full Calendar "
   :url "https://github.com/SVMBrown/ajenda"
   :license {:name "MIT"
--- src/clj/ajenda/assets.clj
+++ src/clj/ajenda/assets.clj
@@ -8,5 +8,5 @@
 (defn ajenda-js []
   [:span
    (include-js "/assets/jquery/jquery.min.js")
-   (include-js "/assets/moment/moment.min.js")
+   (include-js "/assets/moment/min/moment.min.js")
    (include-js "/assets/fullcalendar/fullcalendar.min.js")])
